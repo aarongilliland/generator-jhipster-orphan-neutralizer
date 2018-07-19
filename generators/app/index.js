fa3d700dbaab4b526e02d08046814ae1850e0342
Aaron J Gilliland <Aaron.gilliland@aviture.us.com>
2018-07-19 18:35:23 -0500

    yep another file

---
+++ generators/app/index.js
@@ -0,0 +1,12 @@
+const util = require('util');
+const BaseGenerator = require('generator-jhipster/generators/generator-base');
+const jhipsterConstants = require('generator-jhipster/generators/generator-constants');
+
+const JhipsterGenerator = generator.extend({});
+util.inherits(JhipsterGenerator, BaseGenerator);
+
+module.exports = JhipsterGenerator.extend({
+
+    // all your yeoman code here
+
+});
